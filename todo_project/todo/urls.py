ef7c252106dc6600e0ab1435e2af3b685f84029c
manipatel18 <kummarimanikanta1812@gmail.com>
2026-03-05 17:08:37 +0530

    Added CSS file

--- todo_project/todo/urls.py
+++ todo_project/todo/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('delete/<int:pk>/', views.delete_task, name='delete'),
+    path('edit/<int:id>', views.edit_task, name = 'edit'),
+    path('update/<int:id>', views.update_task, name ='update'),
     path('toggle/<int:pk>/', views.toggle_complete, name='toggle'),
 ]
--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -4,8 +4,9 @@ from .models import Task
 def home(request):
     if request.method == 'POST':
         title = request.POST.get('title')
-        Task.objects.create(title=title)
-        return redirect('home')
+        if title:
+            Task.objects.create(title=title)
+            return redirect('home')
 
     tasks = Task.objects.all()
     return render(request, 'home.html',{'tasks':tasks})
@@ -15,6 +16,28 @@ def delete_task(request, pk):
     task = get_object_or_404(Task,id= pk)
     task.delete()
     return redirect('home')
+
+def edit_task(request, id):
+    task = Task.objects.get(id=id)
+
+    if request.method == "POST":
+        title = request.POST.get('title')
+        
+        if title:
+            task.title = title
+            task.save()
+            return redirect('/')
+    return render(request, 'edit.html',{'task': task})          
+
+
+def update_task(request, id):
+    task = get_object_or_404(Task, id=id)
+
+    if request.method == "POST":
+        task.title = request.POST.get('title')
+        task.save()
+        return redirect('home')
+
 
 def toggle_complete(request, pk):
     task = get_object_or_404(Task, id=pk)
